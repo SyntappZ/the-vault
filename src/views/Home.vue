95cc4b100fe9ca19d6f1969b692d8f879fc90b45
apex viruz <doddsanater87@gmail.com>
2019-04-20 17:59:26 +0100

    done front page login display

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,40 @@
     <v-container fluid class="contianer">
       <v-layout row wrap>
         <v-flex>
-          <div class="box sign-in"></div>
+          <div class="box sign-in">
+             <v-responsive>
+               <v-icon color="secondary" class="lock" >lock</v-icon>
+             
+              <v-container fill-height>
+                <v-layout align-center>
+                  <v-flex>
+                    <form>
+                       <v-text-field
+                        v-model="email"
+                        :error-messages="emailErrors"
+                        label="E-mail"
+                        required
+                        @input="$v.email.$touch()"
+                        @blur="$v.email.$touch()"
+                      ></v-text-field>
+                      <v-text-field
+                        v-model="password"
+                        :error-messages="passwordErrors"
+                        label="Password"
+                        required
+                        @input="$v.password.$touch()"
+                        @blur="$v.password.$touch()"
+                      ></v-text-field>
+                     
+                    </form>
+                    <v-btn flat round class="my-3 secondary" large>Login</v-btn>
+                    <h3>Don't have an account?<span class="sign-up">sign up</span></h3>
+                    
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-responsive>
+          </div>
         </v-flex>
         <v-flex>
           <div class="box welcome">
@@ -22,7 +55,7 @@
 
                     <div class="title mb-3">Check out our newest features!</div>
 
-                    <v-btn flat round class="my-3 primary" large>See more</v-btn>
+                    <v-btn flat round class="my-3 primary" large>create</v-btn>
                   </v-flex>
                 </v-layout>
               </v-container>
@@ -44,13 +77,24 @@ export default {
 .sign-in {
   width: 500px;
   height: 60vh;
-  border: solid 1px #000;
   margin: 100px auto;
 }
 .welcome {
   width: 600px;
   height: 600px;
   margin: 50px auto;
+}
+.sign-up {
+  font-weight: 300;
+  padding-left:15px;
+  cursor: pointer;
+}
+.lock {
+  font-size: 80px;
+  width:120px;
+  height:120px;
+  border:solid 3px;
+  border-radius:50%;
 }
 </style>
 
